2081f84fd3d65753423e53acc92ea1a357b87eb5
Joao Antonio De Morais Malaquias <gameplaysprimos755@gmail.com>
2026-03-01 00:30:12 +0000

    File changes

--- src/components/landing/AurumLogo.jsx
+++ src/components/landing/AurumLogo.jsx
@@ -4,6 +4,7 @@ export default function AurumLogo({ className = "h-12" }) {
       src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69a383af798034b14ce37469/310e79247_WhatsAppImage2026-01-30at1902451.jpg"
       alt="Aurum"
       className={`${className} w-auto object-contain`}
+      style={{ mixBlendMode: "lighten" }}
     />
   );
 }

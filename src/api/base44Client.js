e5cd3bc70a9e2682cfbff164a4fd08ca5b0adbd3
joaozee <148729671+joaozee@users.noreply.github.com>
2026-04-22 18:04:35 -0300

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,14 +1,6 @@
-import { createClient } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
+import { createClient } from '@supabase/supabase-js'
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
+const supabaseUrl = 'https://iaedslcmhsmgbnpogevh.supabase.co'
+const supabaseKey = 'sb_publishable_SiYQoekblKmCYNobyochtw_j3-hy4Ep'
 
-//Create a client with authentication required
-export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
-  requiresAuth: false,
-  appBaseUrl
-});
+export const supabase = createClient(supabaseUrl, supabaseKey)

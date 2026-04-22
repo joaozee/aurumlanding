b7475d104d3630fcedf3c598dcc9cc8aacd9b846
joaozee <148729671+joaozee@users.noreply.github.com>
2026-04-22 19:21:48 -0300

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://iaedslcmhsmgbnpogevh.supabase.co'
-const supabaseKey = 'sb_publishable_SiYQoekblKmCYNobyochtw_j3-hy4Ep'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZWRzbGNtaHNtZ2JucG9nZXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4OTA3MDYsImV4cCI6MjA5MjQ2NjcwNn0.lYOy1jvN5bpIX_ISAvd0g4cSP6iY5ala221zfkpJXtY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
